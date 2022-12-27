5084318daa375e66dd2ce90b513dfc453103f74e
saiyidatul <saiyidatul@kloena.com>
2022-12-27 16:40:12 +0800

    Test minor changes

--- js/addToCart.js
+++ js/addToCart.js
@@ -1,36 +1,33 @@
-$(document).on("ready", function () {
-  
-  $(".plus-btn").click(function (e) {
-    e.preventDefault();
+$(".plus-btn").click(function (e) {
+  e.preventDefault();
 
-    alert("hello");
+  alert("hello");
 
-    var qty = $(this).closest(".product_data").find(".qty-input").val();
+  var qty = $(this).closest(".product_data").find(".qty-input").val();
 
-    var value = parseInt(qty, 10);
-    value = isNaN(value) ? 0 : value;
+  var value = parseInt(qty, 10);
+  value = isNaN(value) ? 0 : value;
 
-    if (value < 10) {
-      value++;
-      $(".qty-input").val(value);
-      $(this).closest(".product_data").find(".qty-input").val(value);
-    }
-  });
+  if (value < 10) {
+    value++;
+    $(".qty-input").val(value);
+    $(this).closest(".product_data").find(".qty-input").val(value);
+  }
+});
 
-  $(".minus-btn").click(function (e) {
-    e.preventDefault();
+$(".minus-btn").click(function (e) {
+  e.preventDefault();
 
-    alert("hello");
+  alert("hello");
 
-    var qty = $(this).closest(".product_data").find(".qty-input").val();
+  var qty = $(this).closest(".product_data").find(".qty-input").val();
 
-    var value = parseInt(qty, 10);
-    value = isNaN(value) ? 0 : value;
+  var value = parseInt(qty, 10);
+  value = isNaN(value) ? 0 : value;
 
-    if (value > 1) {
-      value--;
-      $(".qty-input").val(value);
-      $(this).closest(".product_data").find(".qty-input").val(value);
-    }
-  });
+  if (value > 1) {
+    value--;
+    $(".qty-input").val(value);
+    $(this).closest(".product_data").find(".qty-input").val(value);
+  }
 });
