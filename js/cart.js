693bf7070d28cc83959721770ff0e0d8736eabc2
saiyidatul <saiyidatul@kloena.com>
2023-03-21 10:51:22 +0800

    latest commit

---
+++ js/cart.js
@@ -0,0 +1,14 @@
+var shoppingCart = (function (){
+
+    // Declare empty cart array
+    cart = [];
+
+    // Constructor
+    function Item(name, price, count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    
+})
